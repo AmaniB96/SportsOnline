3d2c1daa068cec6cb5c1cc32b35eeca8c4bea190
kottor <gardien243@gmail.com>
2025-09-02 10:05:49 +0200

    au moins j'ai reucup ce que j'ai fais

--- app/Http/Controllers/JoueurController.php
+++ app/Http/Controllers/JoueurController.php
@@ -6,5 +6,23 @@ use Illuminate\Http\Request;
 
 class JoueurController extends Controller
 {
-    //
+    public function index(){
+        return view('back.player');
+    }
+    public function show($id){
+        return view('back.player_show');
+    }
+    public function create(){
+        return view('back.player_create');
+    }
+    public function store(){
+        
+        return redirect()->Route('back.player');
+    }
+    public function destroy($id){
+        return redirect()->Route('back.player');
+    }
+    public function update($id){
+        return redirect()->Route('back.player');
+    }
 }
