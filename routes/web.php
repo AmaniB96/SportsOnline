809ccbca959946fc46922e7e9167dc9806653bbb
kottor <gardien243@gmail.com>
2025-09-02 11:30:00 +0200

    j'ai commencer la nav back

--- routes/web.php
+++ routes/web.php
@@ -2,18 +2,19 @@
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\EquipeController;
+use App\Http\Controllers\homeController;
 use App\Http\Controllers\JoueurController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\RoleController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/', [homeController::class, 'index'])->name('home');
 Route::get('/equipe', [HomeController::class, 'equipe'])->name('home.equipe');
 Route::get('/show/{id}', [HomeController::class, 'show'])->name('home.show');
 
 Route::resource('back/player', JoueurController::class)->names('back.player');
 Route::resource('back/equipe', EquipeController::class)->names('back.equipe');
-Route::resource('back/user',UserController::class)->names('back.user');
+Route::resource('back/user',ProfileController::class)->names('back.user');
 
 
 Route::get('/dashboard', function () {
