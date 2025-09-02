6bafbff09fd478ffe72fb9be97cacc6eb1c17626
kottor <gardien243@gmail.com>
2025-09-02 15:16:07 +0200

    j'en ai marre

---
+++ resources/views/layouts/front_layout.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title', 'Sports Online')</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+    <header>
+        <nav>
+            <a href="{{ route('home') }}">Accueil</a>
+            <a href="{{ route('home.equipe') }}">Équipes</a>
+        </nav>
+    </header>
+
+    <main>
+        @yield('content')
+    </main>
+
+    <footer>
+        <p>SportsOnline. Tous droits réservés.</p>
+    </footer>
+</body>
+</html>
