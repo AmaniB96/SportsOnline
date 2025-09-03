e373feac2a17f03c408a262e0cd6786cc4a1a6fb
kottor <gardien243@gmail.com>
2025-09-04 01:33:21 +0200

    gate preparer

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Gate::define("isAdmin", function ($user) {
+            return $user->role_id == 3;
+        });
+        Gate::define("isAdminorCoach", function ($user) {
+            return in_array($user->role_id, [2, 3]);
+        });
+        Gate::define("isCoach", function ($user) {
+            return $user->role_id == 2;
+        });
     }
 }
