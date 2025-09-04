64610a9a5e453e146a0be77dc5ef57f777c2e7e2
kottor <gardien243@gmail.com>
2025-09-04 09:59:03 +0200

    connexion ok

--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -1,7 +1,7 @@
 <section>
     <header>
         <h2 class="text-lg font-medium text-gray-900">
-            {{ __('Profile Information') }}
+            {{ __('information du profile') }}
         </h2>
 
         <p class="mt-1 text-sm text-gray-600">
@@ -36,16 +36,16 @@
             @if ($user instanceof \Illuminate\Contracts\Auth\MustVerifyEmail && ! $user->hasVerifiedEmail())
                 <div>
                     <p class="text-sm mt-2 text-gray-800">
-                        {{ __('Your email address is unverified.') }}
+                        {{ __("Votre adresse n'est pas verifier") }}
 
                         <button form="send-verification" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                            {{ __('Click here to re-send the verification email.') }}
+                            {{ __('clic ici pour relancer la verification') }}
                         </button>
                     </p>
 
                     @if (session('status') === 'verification-link-sent')
                         <p class="mt-2 font-medium text-sm text-green-600">
-                            {{ __('A new verification link has been sent to your email address.') }}
+                            {{ __('une nouvel verfication a ete lancer  avotre adresse') }}
                         </p>
                     @endif
                 </div>
@@ -53,7 +53,7 @@
         </div>
 
         <div class="flex items-center gap-4">
-            <x-primary-button>{{ __('Save') }}</x-primary-button>
+            <x-primary-button>{{ __('mise a jour') }}</x-primary-button>
 
             @if (session('status') === 'profile-updated')
                 <p
@@ -62,7 +62,7 @@
                     x-transition
                     x-init="setTimeout(() => show = false, 2000)"
                     class="text-sm text-gray-600"
-                >{{ __('Saved.') }}</p>
+                >{{ __('Sauvegarder.') }}</p>
             @endif
         </div>
     </form>
