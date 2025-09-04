2af5743c4f6e3813d8aaca87f69226edf21365d3
kottor <gardien243@gmail.com>
2025-09-04 21:33:35 +0200

    mise en relation inscription,connexion

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,7 +6,8 @@
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
-    <form method="POST" action="{{ route('login') }}">
+    <form method="POST" action="{{ route('login') }}" class="mt-32 mx-32">
+        <h1 class="mb-5 font-extrabold text-5xl">Connexion</h1>
         @csrf
 
         <!-- Email Address -->
@@ -35,11 +36,14 @@
         </div>
 
         <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
+            {{-- @if (Route::has('password.request'))
                 <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
                     {{ __('Forgot your password?') }}
                 </a>
-            @endif
+            @endif --}}
+            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('register') }}">
+                {{ __('Pas de compte') }}
+            </a>
 
             <x-primary-button class="ms-3">
                 {{ __('Log in') }}
